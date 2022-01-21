415dbd8cbe6d4ba9a1afa65982d35f2aa562bbb7
Victor Jr Garcia Montalvan <86797211+vgar98@users.noreply.github.com>
2022-01-21 13:01:27 -0500

    !importante update

--- DatetimeToDateJS.js
+++ DatetimeToDateJS.js
@@ -1,9 +1,8 @@
-
-function getDateFromDatetime(fecha){
+function retorna_fecha_ymd(fecha){
     let new_fecha = new Date(fecha);
     let anho = new_fecha.getFullYear();
     let dia = new_fecha.getDate();
     let mes = new_fecha.getMonth()+1;
-    let fecha_nueva = `${anho}-${(mes+''.length<2?'0'+mes:mes)}-${(dia+''.length<2?'0'+dia:dia)}`;
+    let fecha_nueva = `${anho}-${((mes+'').length<2?'0'+mes:mes)}-${((dia+'').length<2?'0'+dia:dia)}`;
     return fecha_nueva;
 }
